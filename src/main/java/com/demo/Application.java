97ccfe0cce580b8d0778409fb6a0d45a373b1332
nikkinicholasromero <nikkinicholas.romero@gmail.com>
2023-06-22 09:55:39 +0800

    Showed SQL logs

--- src/main/java/com/demo/Application.java
+++ src/main/java/com/demo/Application.java
@@ -6,11 +6,11 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@Component
+@RestController
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
